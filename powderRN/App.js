fcc3dbee956870e942302d96aaa1309904a29f59
Kevin Haro <harokevin0@gmail.com>
2017-12-17 12:08:20 -0800

    Mission ridge working

--- powderRN/App.js
+++ powderRN/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { report: null };
+    this.state = { isLoading: true, oldRunCode: "teBk3g2Tv_2B", latestRunCode: "tKWREbcsOqdJ" };
   }
 
   componentDidMount() {
-    console.log("From componentDidMount");
-    return fetch("https://www.parsehub.com/api/v2/runs/teBk3g2Tv_2B/data?api_key=toxisgTKgB50")
+    return fetch("https://www.parsehub.com/api/v2/runs/"+this.state.latestRunCode+"/data?api_key=toxisgTKgB50")
       .then((response) => { return response.json(); })
       .then((responseJson) => {
-        this.setState({ report: responseJson.toString(),
-                        json: responseJson,
-                      });
         let js = responseJson;
         let mr = js.MissionRidge;
-        
-        let resort = new Object();
-        resort["name"] = "Mission Ridge";
+
+        let resort = {};
+        resort.name = "Mission Ridge";
         resort.metrics = [];
         let i = 0;
         for (i=0; i < mr.length; i++) {
@@ -36,23 +32,107 @@ export default class App extends React.Component {
             mobj.Midway = metric.Midway;
             resort[metric_name] = mobj;
             resort.metrics.push(metric_name);
-            console.log(mobj);
         }
-        this.setState({ resort: resort });
 
+        this.setState({ isLoading: false, resort: resort, name: resort.name, Overnight: resort.Overnight.Summit, _24_Hours: resort["24 hrs"].Summit, _48_Hours: resort["48 hrs"].Summit  });
         console.log(resort);
+
       })
       .catch((error) => {
         console.error(error);
-      });;
+      });
   }
 
   render() {
-    return (
+
+    if(this.state.isLoading) {
+      return (
+        <View style={{flex: 1}}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+          <View style={{flex: 2, backgroundColor: 'skyblue'}}>
+            <ActivityIndicator size="large" color="white" style={{flex:1, flexDirection:'row', alignItems:'center', justifyContent:'center'}}/>
+          </View>
+          <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+        </View>
+      );
+    }
+
+    return(
       <View style={styles.container}>
-        <Text>report: {this.state.report}</Text>
+
+        <View style={{width:450, height: 140, paddingBottom: 5}}>
+          <Text style={{textAlign:'center'}}>{this.state.name}</Text>
+          <View style={{flex: 1, flexDirection: 'row'}} >
+            <View style={{flex:1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>Overnight</Text></View>
+            <View style={{flex:1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>24 Hours</Text></View>
+            <View style={{flex:1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>48 Hours</Text></View>
+          </View>
+          <View style={{flex: 2, flexDirection: 'row'}}>
+            <View style={{flex: 1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state.Overnight}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._24_Hours}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._48_Hours}</Text></View>
+          </View>
+        </View>
+
+        <View style={{width:450, height: 140, paddingBottom: 5}}>
+          <Text style={{textAlign:'center'}}>{this.state.name}</Text>
+          <View style={{flex: 1, flexDirection: 'row'}} >
+            <View style={{flex:1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>Overnight</Text></View>
+            <View style={{flex:1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>24 Hours</Text></View>
+            <View style={{flex:1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>48 Hours</Text></View>
+          </View>
+          <View style={{flex: 2, flexDirection: 'row'}}>
+            <View style={{flex: 1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state.Overnight}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._24_Hours}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._48_Hours}</Text></View>
+          </View>
+        </View>
+
+        <View style={{width:450, height: 140, paddingBottom: 5}}>
+          <Text style={{textAlign:'center'}}>{this.state.name}</Text>
+          <View style={{flex: 1, flexDirection: 'row'}} >
+            <View style={{flex:1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>Overnight</Text></View>
+            <View style={{flex:1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>24 Hours</Text></View>
+            <View style={{flex:1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>48 Hours</Text></View>
+          </View>
+          <View style={{flex: 2, flexDirection: 'row'}}>
+            <View style={{flex: 1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state.Overnight}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._24_Hours}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._48_Hours}</Text></View>
+          </View>
+        </View>
+
+        <View style={{width:450, height: 140, paddingBottom: 5}}>
+          <Text style={{textAlign:'center'}}>{this.state.name}</Text>
+          <View style={{flex: 1, flexDirection: 'row'}} >
+            <View style={{flex:1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>Overnight</Text></View>
+            <View style={{flex:1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>24 Hours</Text></View>
+            <View style={{flex:1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>48 Hours</Text></View>
+          </View>
+          <View style={{flex: 2, flexDirection: 'row'}}>
+            <View style={{flex: 1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state.Overnight}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._24_Hours}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._48_Hours}</Text></View>
+          </View>
+        </View>
+
+        <View style={{width:450, height: 140, paddingBottom: 5}}>
+          <Text style={{textAlign:'center'}}>{this.state.name}</Text>
+          <View style={{flex: 1, flexDirection: 'row'}} >
+            <View style={{flex:1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>Overnight</Text></View>
+            <View style={{flex:1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>24 Hours</Text></View>
+            <View style={{flex:1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>48 Hours</Text></View>
+          </View>
+          <View style={{flex: 2, flexDirection: 'row'}}>
+            <View style={{flex: 1, backgroundColor: 'powderblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state.Overnight}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'skyblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._24_Hours}</Text></View>
+            <View style={{flex: 1, backgroundColor: 'steelblue', justifyContent:'center'}}><Text style={{textAlign:'center'}}>{this.state._48_Hours}</Text></View>
+          </View>
+        </View>
+
       </View>
     );
+
   }
 }
 
@@ -62,5 +142,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 40
   },
 });
