df200c8f406df2b0a7c1233d1f1dece367419c33
Lance Kindle <lance.kindle@gmail.com>
2017-12-17 01:12:07 -0800

    add json parsed object for MR resort

--- powderRN/App.js
+++ powderRN/App.js
@@ -11,10 +11,36 @@ export default class App extends React.Component {
   componentDidMount() {
     console.log("From componentDidMount");
     return fetch("https://www.parsehub.com/api/v2/runs/teBk3g2Tv_2B/data?api_key=toxisgTKgB50")
-      .then((response) => { console.log(response); return response.json(); })
+      .then((response) => { return response.json(); })
       .then((responseJson) => {
-        this.setState({ report: responseJson.toString() })
-        console.log(responseJson);
+        this.setState({ report: responseJson.toString(),
+                        json: responseJson,
+                      });
+        let js = responseJson;
+        let mr = js.MissionRidge;
+        
+        let resort = new Object();
+        resort["name"] = "Mission Ridge";
+        resort.metrics = [];
+        let i = 0;
+        for (i=0; i < mr.length; i++) {
+            metric = mr[i];
+            metric_name = metric["metric"];
+            if (["48 hrs", "24 hrs", "Overnight"].includes(metric_name) == false) {
+                continue;
+            }
+            let mobj = new Object();
+            mobj["name"] = metric["metric"];
+            mobj.Base = metric.Base;
+            mobj.Summit = metric.Summit;
+            mobj.Midway = metric.Midway;
+            resort[metric_name] = mobj;
+            resort.metrics.push(metric_name);
+            console.log(mobj);
+        }
+        this.setState({ resort: resort });
+
+        console.log(resort);
       })
       .catch((error) => {
         console.error(error);
